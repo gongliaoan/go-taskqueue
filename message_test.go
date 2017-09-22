4434660d73794fa1e15be417fdb66ea4f7b9e5f4
Eder Ruiz Maria <drrzmr@gmail.com>
2017-09-22 14:41:12 -0300

    test: Add timeout tests

--- message_test.go
+++ message_test.go
@@ -94,3 +94,63 @@ func Test_messageWithTimeout(t *testing.T) {
 	}
 	require.True(t, cancel)
 }
+
+// begin message timeout
+type taskTestMessageTimeout struct {
+	timeout bool
+}
+
+func (task *taskTestMessageTimeout) Success(queueID, taskID string) {
+	task.timeout = false
+}
+
+func (task *taskTestMessageTimeout) Timeout(queueID, taskID string) {
+	task.timeout = true
+}
+
+func Test_messageTimeout(t *testing.T) {
+
+	var (
+		messageChannelSend chan<- *message
+		deleteChannelRecv  <-chan Notification
+	)
+
+	messageChannel := make(chan *message, 1)
+	deleteChannel := make(chan Notification)
+
+	messageChannelSend = messageChannel
+	deleteChannelRecv = deleteChannel
+
+	// force timeout by hand
+	timeoutHandle := func(cancelCh chan<- Notification, timeout time.Duration) *time.Timer {
+		close(cancelCh)
+		return nil
+	}
+
+	task := &taskTestMessageTimeout{
+		timeout: false,
+	}
+
+	doneCh, timeoutCh, msg := newMessageWithTimeoutHandleFunc(time.Duration(1), task, timeoutHandle)
+	require.NotNil(t, msg)
+	require.Nil(t, msg.timer)
+
+	go consumer(messageChannel, deleteChannel, "messageTimeout")
+
+	messageChannelSend <- msg
+	close(messageChannel)
+	<-deleteChannelRecv
+
+	var timeout bool
+	select {
+	case <-doneCh:
+		timeout = false
+	case <-timeoutCh:
+		timeout = true
+	}
+
+	require.True(t, timeout)
+	require.True(t, task.timeout)
+}
+
+// end message timeout
